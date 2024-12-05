c5971be6261c90ee87d3b5742ade8b4b7e0fd72d
Yoshiaki Iwamoto <yoshiaki.iwamoto@gmail.com>
2024-12-05 18:21:10 +0900

    マイページの追加

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <nav>
       <router-link to="/">ホーム</router-link> |
       <router-link to="/about">アバウト</router-link> | 
-      <router-link to="/edit">作成</router-link>
+      <router-link to="/edit">作成</router-link>|
+      <router-link to="/mypage">マイページ</router-link>
     </nav>
     <router-view></router-view>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/HomeView.vue';
 import About from '../views/AboutView.vue';
 import Edit from '../views/EditTier.vue';
+import Mypage from '../views/MyPage.vue';
 
 const routes = [
   { path: '/', component: Home },   // ホームページ
   { path: '/about', component: About }, // アバウトページ
-  { path: '/edit', component: Edit }
+  { path: '/edit', component: Edit },
+  { path: '/mypage', component: Mypage}
 ];
 
 const router = createRouter({
