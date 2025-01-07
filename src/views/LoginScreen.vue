b17988d1cfb4272537cda83429f4f78fd6a32fe2
Yoshiaki Iwamoto <yoshiaki.iwamoto@gmail.com>
2025-01-07 10:35:49 +0900

    ログインページの修正

--- src/views/LoginScreen.vue
+++ src/views/LoginScreen.vue
@@ -6,54 +6,49 @@
     </div>
   </template>
   
-  <script>
-  import { firebase } from "@/firebase/init.js"; // Firebaseの初期化をインポート
-  import * as firebaseui from "firebaseui";
-  import "firebaseui/dist/firebaseui.css";
-  import { getAuth, onAuthStateChanged } from "firebase/auth";
-  import { useRouter } from 'vue-router';
-  
-  export default {
-    name: "LoginScreen",
-    mounted() {
-      this.createUI();
-      this.checkAuthState();
-    },
-    methods: {
-      createUI() {
-        const auth = getAuth();
-        const ui = new firebaseui.auth.AuthUI(auth);
-        ui.start("#firebaseui-auth-container", {
-          callbacks: {
-            signInSuccessWithAuthResult: (authResult, redirectUrl) => {
-              console.log("ログイン成功");
-              this.$router.push('/home');
-              return false;
-            },
-            uiShown: () => {
-              document.getElementById("loader").style.display = "none";
-            },
-          },
-          signInFlow: "popup",
-          signInOptions: [
-            firebase.auth.GoogleAuthProvider.PROVIDER_ID
-          ],
-        });
-      },
-      checkAuthState() {
-        const auth = getAuth();
-        onAuthStateChanged(auth, (user) => {
-          if (user) {
-            console.log("User is logged in:", user);
-            this.$router.push('/home');
-          } else {
-            console.log("User is not logged in");
-          }
-        });
-      }
-    }
-  };
-  </script>
+<script>
+    import * as firebaseui from 'firebaseui';
+    import 'firebaseui/dist/firebaseui.css';
+    import { firebase  } from "@/firebase/init.js"; 
+    export default {
+        name: "LoginScreen",
+        mounted(){
+            this.createUI();
+        },
+        methods: {
+            
+            createUI(){ 
+                const ui = new firebaseui.auth.AuthUI(firebase.auth());
+                const uiConfig = {
+                    callbacks: {
+                        signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+                        // User successfully signed in.
+                        // Return type determines whether we continue the redirect automatically
+                        // or whether we leave that to developer to handle.
+                        return true;
+                        },
+                        uiShown: function() {
+                        // The widget is rendered.
+                        // Hide the loader.
+                        document.getElementById('loader').style.display = 'none';
+                        }
+                    },
+                    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+                    signInFlow: 'popup',
+                    signInSuccessUrl: '/home',
+                    signInOptions: [
+                        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                    ],
+
+                }
+                ui.start('#firebaseui-auth-container', uiConfig);
+
+            }   
+            
+        }
+    };
+    
+</script>
   
   <style scoped>
   </style>
